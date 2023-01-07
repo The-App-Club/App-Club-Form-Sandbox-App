9a5b480af0bd04cbb63be89d51934ce5811dfc65
Higashi-Kota <higashi.kota@gri.jp>
2023-01-07 12:38:52 +0900

    add sample

---
+++ react-hook-form-basic-handling-nextjs-typescript-app/src/config/headerMenu.ts
@@ -0,0 +1,29 @@
+export type HeaderMenu = {
+  id: number
+  name: string
+  url: string
+  canonicalURL: string | null
+}
+
+const headerMenus: HeaderMenu[] = [
+  {
+    id: 1,
+    name: 'Issue',
+    url: '/issues',
+    canonicalURL: null,
+  },
+  {
+    id: 2,
+    name: 'Users',
+    url: '/users',
+    canonicalURL: null,
+  },
+  {
+    id: 3,
+    name: 'Liquors',
+    url: '/liquors',
+    canonicalURL: null,
+  },
+]
+
+export { headerMenus }
